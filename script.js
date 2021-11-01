79bb420074f801f63c570153dfebc92f2b2154ae
drougstar <sizofko@gmail.com>
2021-11-02 01:01:56 +0300

    main

--- script.js
+++ script.js
@@ -1,45 +1,94 @@
-'use strict';
+"use strict";
 
 // Selecting Elements
-const scoreP0 = document.querySelector('#score--0'),
-  scoreP1 = document.getElementById('score--1'),
-  currentP0 = document.querySelector('#current--0'),
-  currentP1 = document.querySelector('#current--1'),
-  dice = document.querySelector('.dice'),
-  buttonRoll = document.querySelector('.btn--roll'),
-  checkDice = function (diceNumber, current) {
-    // Checking if the dice is 1 or not /  if 1 then return the value to 0
-    if (diceNumber !== 1) {
-      current.textContent = Number(current.textContent) + diceNumber;
-    } else {
-      current.textContent = 0;
-      current = switchPlayer(current);
-    }
-    return current;
+const scoreP0 = document.querySelector("#score--0"),
+  scoreP1 = document.getElementById("score--1"),
+  currentP0 = document.querySelector("#current--0"),
+  currentP1 = document.querySelector("#current--1"),
+  nameP0 = document.querySelector("#name--0"),
+  nameP1 = document.querySelector("#name--1"),
+  player0 = document.querySelector(".player--0"),
+  player1 = document.querySelector(".player--1"),
+  dice = document.querySelector(".dice"),
+  buttonNew = document.querySelector(".btn--new"),
+  buttonRoll = document.querySelector(".btn--roll"),
+  buttonHold = document.querySelector(".btn--hold"),
+  // Switching players according to the reason
+  switchStyle = function (playernew) {
+    player.classList.remove("player--active");
+    player = playernew;
+    player.classList.add("player--active");
   },
-  switchPlayer = function (current) {
-    if (current === currentP0) {
-      return currentP1;
-    } else return currentP0;
-  };
-let currentPlayer = currentP0;
+  switchPlayer = function () {
+    if (currentPlayer === currentP0) {
+      {
+        switchStyle(player1);
 
+        currentScore = scoreP1;
+        return currentP1;
+      }
+    } else {
+      switchStyle(player0);
+
+      currentScore = scoreP0;
+      return currentP0;
+    }
+  },
+  // Checking if the dice is 1 or not /  if 1 then return the value to 0
+  checkDice = function () {
+    if (diceNumber !== 1) {
+      currentPlayer.textContent =
+        Number(currentPlayer.textContent) + diceNumber;
+    } else {
+      currentPlayer.textContent = 0;
+      currentPlayer = switchPlayer();
+    }
+    return currentPlayer;
+  },
+  // Starting values
+  starting = function () {
+    currentP0.textContent = 0;
+    currentP1.textContent = 0;
+    scoreP0.textContent = 0;
+    scoreP1.textContent = 0;
+    currentPlayer = currentP0;
+    currentScore = scoreP0;
+    player = player0;
+    player0.classList.add("player--active");
+    player1.classList.remove("player--active");
+    player0.classList.remove("player--winner");
+    player1.classList.remove("player--winner");
+    diceNumber = 0;
+    diceNumberStr = "";
+    dice.classList.add("hidden");
+  };
+let currentPlayer, currentScore, player, diceNumber, diceNumberStr;
 // Starting Conditions
-currentP0.textContent = 0;
-currentP1.textContent = 0;
-scoreP0.textContent = 0;
-scoreP1.textContent = 0;
-dice.classList.add('hidden');
+starting();
 
 // Rolling Dice When clicked
-buttonRoll.addEventListener('click', function () {
+buttonRoll.addEventListener("click", function () {
   // Creating a random variable
-  let diceNumber = Math.trunc(Math.random() * 6) + 1,
-    diceNumberStr = `dice-${diceNumber}.png`;
+  diceNumber = Math.trunc(Math.random() * 6) + 1;
+  diceNumberStr = `dice-${diceNumber}.png`;
   // Calling an image and revealing dice
-  document.querySelector('.dice').src = diceNumberStr;
-  dice.classList.remove('hidden');
+  dice.src = diceNumberStr;
+  dice.classList.remove("hidden");
 
   // Run function to check if dice is not 1
-  currentPlayer = checkDice(diceNumber, currentPlayer);
+  currentPlayer = checkDice();
 });
+
+// Holding Score
+buttonHold.addEventListener("click", function () {
+  currentScore.textContent =
+    Number(currentScore.textContent) + Number(currentPlayer.textContent);
+  currentPlayer.textContent = 0;
+  if (Number(currentScore.textContent) >= 100) {
+    player.classList.add("player--winner");
+    dice.classList.add("hidden");
+  } else currentPlayer = switchPlayer();
+});
+
+// Newgame
+buttonNew.addEventListener("click", starting);
